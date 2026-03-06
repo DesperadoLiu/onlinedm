04ddf3785f971ed2d5e1d8d451e2b1942562ee4d
DesperadoLiu <141300152+DesperadoLiu@users.noreply.github.com>
2026-03-06 21:44:00 +0800

    Add files via upload

---
+++ despo/tailwind-config.js
@@ -0,0 +1,14 @@
+window.tailwind = window.tailwind || {};
+window.tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                brand: {
+                    orange: '#E87033',
+                    yellow: '#E38827',
+                    dark: '#2D2926',
+                },
+            },
+        },
+    },
+};
